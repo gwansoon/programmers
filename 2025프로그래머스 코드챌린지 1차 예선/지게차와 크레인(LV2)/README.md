25bf3cf6d9afbefc45f509657b3e8c0f3fac80ad
gwansoon <zx6849@naver.com>
2025-12-14 14:03:37 +0900

    지게차와 크레인

---
+++ 2025프로그래머스 코드챌린지 1차 예선/지게차와 크레인(LV2)/README.md
@@ -0,0 +1,11 @@
+# 문제 제목 
+## 지게차와 크레인
+
+# 문제 링크
+## https://school.programmers.co.kr/learn/courses/30/lessons/388353
+
+# 문제 유형
+## BFS
+
+# 문제 난이도
+## Level 2
---
+++ 2025프로그래머스 코드챌린지 1차 예선/지게차와 크레인(LV2)/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+       public int solution(String[] storage, String[] requests) {
+        int answer = 0;
+        return answer;
+    }
+}
